9e637c6b2cea256b8356164e8625d6a28ea05160
martingratt <martin_gratt@hotmail.com>
2017-07-02 15:07:00 +0200

    master ornder gelöscht?

--- webproject_semester2-master/hangman/db_details.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $server = "127.0.0.1";
-    $db = "Hangman";
-    $user = "root";
-    $pw = ""; /* ?> und leere Zeile wurde wie besprochen entfernt*/
--- webproject_semester2-master/hangman/js/login.js
+++
@@ -1,5 +0,0 @@
-/*$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});*/
-
-//für das Design des Formulares wurde eine Template aus dem Internet verwendet und umgebaut, sodass die Datei login.js nicht benötigt wird
